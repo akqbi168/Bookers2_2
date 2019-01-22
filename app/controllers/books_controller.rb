0159b93e6ec6df0ffda271e7fd3f1a70ea11de5a
akqbi168 <akqbi168@gmail.com>
2019-01-22 19:17:50 +0900

    updated

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+
+    def home
+    end
+
+    def about
+    end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        if @book.save
+            redirect_to books_path
+        else
+            render :new
+        end
+    end
+
+    def index
+        @user = current_user
+        @books = Book.page(params[:page]).reverse_order
+    end
+
+    def show
+        @user = User.find(params[:id])
+        @book = Book.find(params[:id])
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
+
+    private
+        def book_params
+            params.require(:book).permit(:title, :body)
+        end
+
+end
