2f60a1af0ccd59eec225a8de16f5217eebef4c16
akqbi168 <akqbi168@gmail.com>
2019-01-23 17:52:05 +0900

    updated

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1833046a62780873549c55254e78207d946f41c505825b0b8a276e8fbf1d1aaaff23bace90df2e4f427112f01b65e80b9a14b75693bd547f40347820e9b97eb7'
